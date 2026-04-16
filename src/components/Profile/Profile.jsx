e312cbb4ba8e91f66a543886facb48b787aaf81d
CodyMarsee <cmarsee@alumni.iu.edu>
2026-04-16 08:22:08 -0400

    1st Review Update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import "./Profile.css";
 import ClothesSection from "../ClothesSection/ClothesSection";
 import SideBar from "../SideBar/SideBar";
 
-export default function Profile({ clothingItems, onCardClick }) {
+export default function Profile({ clothingItems, onCardClick, onAddClick }) {
   return (
     <section className="profile">
       <SideBar />
--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -2,7 +2,7 @@ import { handleServerResponse } from "./api.js";
 
 export const getWeather = ({ latitude, longitude }, apiKey) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`,
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`,
   ).then(handleServerResponse);
 };
 
